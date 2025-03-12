a40c919516acb4700c80bc377a49a82894a711e5
unknown <hoang0403ss@gmail.com>
2025-03-12 10:31:15 +0700

    chua lam gi ca

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,17 +1,17 @@
-package controllers;  // Đảm bảo package này khớp với khai báo trong FXML
-
-import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
-public class LoginController {  // Đảm bảo class phải là public
-    @FXML private TextField usernameField;
-    @FXML private PasswordField passwordField;
-
-    @FXML
-    private void handleLogin() {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        System.out.println("Đăng nhập với: " + username + " - " + password);
-    }
-}
+//package controllers;  // Đảm bảo package này khớp với khai báo trong FXML
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.PasswordField;
+//import javafx.scene.control.TextField;
+//
+//public class LoginController {  // Đảm bảo class phải là public
+//    @FXML private TextField usernameField;
+//    @FXML private PasswordField passwordField;
+//
+//    @FXML
+//    private void handleLogin() {
+//        String username = usernameField.getText();
+//        String password = passwordField.getText();
+//        System.out.println("Đăng nhập với: " + username + " - " + password);
+//    }
+//}
